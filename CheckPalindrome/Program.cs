c9385c54077a46b462b5af26ed867be3ec267737
Artak Abrahamyan <abrahamyan@outlook.com>
2021-05-02 17:25:41 +0400

    Add project files.

---
+++ CheckPalindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CheckPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool CheckPalindrome(string inputString)
+            {
+                int j = inputString.Length - 1;
+                for (int i = 0; i <= j; i++)
+                {
+                    if (inputString[i] != inputString[j])
+                    {
+                        return false;
+                    }
+                    j--;
+                }
+                return true;
+            }
+
+            Console.WriteLine(CheckPalindrome("aabaa"));
+        }
+    }
+}
