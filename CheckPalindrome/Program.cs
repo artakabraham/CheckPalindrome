977ca42aca7a4aed4ee3c4b30977608646a68889
Artak <abrahamyan@outlook.com>
2021-05-13 10:50:17 +0400

    Update Program.cs

--- CheckPalindrome/Program.cs
+++ CheckPalindrome/Program.cs
@@ -4,6 +4,11 @@ namespace CheckPalindrome
 {
     class Program
     {
+        /// <summary>
+        /// For inputString = "aabaa", the output should be checkPalindrome(inputString) = true; 
+        /// For inputString = "abac", the output should be checkPalindrome(inputString) = false;
+        /// For inputString = "a", the output should be checkPalindrome(inputString) = true.
+        /// </summary>
         static void Main(string[] args)
         {
             bool CheckPalindrome(string inputString)
